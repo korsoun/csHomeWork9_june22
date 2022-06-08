b0f43b67ddb2d1df9b74bb308f6bd00df6a85702
alexkors <korsun55555@mail.ru>
2022-06-08 19:23:37 +0700

    вывод чисел от N до 1

---
+++ 64/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать N.
+// Вывести с помощью рекурсии натуральные числа от N до 1.
+
+void GetNumberRowRecursively(int n, int N)
+{
+    
+    if (n == 0) return;
+
+    GetNumberRowRecursively(n - 1, N);
+    Console.Write($"{(N+1)-n} ");
+}
+
+Console.WriteLine("Задайте число N: ");
+int n =  Convert.ToInt32(Console.ReadLine());
+int N = n;
+GetNumberRowRecursively(n, N);
